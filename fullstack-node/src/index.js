4a2194a553310df601515b73f10977276b764744
Wady <wadyprograming@gmail.com>
2026-01-25 09:54:18 -0600

    Added middleware routing logic and new APIS

--- fullstack-node/src/index.js
+++ fullstack-node/src/index.js
@@ -1,11 +1,39 @@
-const express = require('express');
-
+const express = require('express');const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
 const tasksRouter = require('./tasks/tasks.router');
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
 
+app.use(express.json());
+
+//This is how to enable CORS for specific origin
+// const corsOptions  = {
+//     origin: 'http://localhost:3000',
+//     optionsSuccessStatus: 200 
+// };
+
+//app.use(cors(corsOptions)); 
+app.use(cors()); //This is to enable CORS for all routes is a security risk in production
+
+let accessLogStream = fs.createWriteStream(
+    path.join(__dirname, "..","access.log"), 
+    {
+        flags: 'a',
+    } 
+);
+
+// Middleware to parse JSON bodies
+// const middleWare = function (req, res, next) {
+//     req.info = { appname: 'Task Manager', author: 'Wady' };
+//     next();
+// }
+// app.use(middleWare);
+app.use(morgan('combined', { stream: accessLogStream}));
 app.use('/', tasksRouter);
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`App listening on port no: ${port}`);
--- fullstack-node/src/tasks/tasks.router.js
+++ fullstack-node/src/tasks/tasks.router.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const tasksRouter = express.Router();
 
-tasksRouter.get('/tasks', (req, res) => res.send('Get all tasks'));
-tasksRouter.post('/tasks', (req, res) => res.send('Create a new task'));
-// tasksRouter.put('/tasks/:id', (req, res) => res.send(`Update task with id ${req.params.id}`));
-// tasksRouter.delete('/tasks/:id', (req, res) => res.send(`Delete task with id ${req.params.id}`));
+// Define routes for tasks
+// tasksRouter.get('/tasks', (req, res) => res.send('Get all tasks'));
+//This endpoint uses the middleware to send info about the app
+tasksRouter.get('/tasks', (req, res) => { 
+    // Accessing the info added by middleware
+    // console.log(req.info);
+    // return res.send(req.info)
+    return res.send('Hello World');
+});
+
+// New POST endpoint to create a task
+// tasksRouter.post('/tasks', (req, res) => res.send('Create a new task'));
+tasksRouter.post('/tasks', (req, res) => {
+    console.log(req.body);
+    console.log(typeof req.body);
+    return res.send("Create a new task");
+});
 
 module.exports = tasksRouter;
