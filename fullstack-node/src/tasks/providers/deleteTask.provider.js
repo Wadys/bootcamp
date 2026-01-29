cb57b279bec521cc890db7f959464da3bd0a4c2d
Wady <wadyprograming@gmail.com>
2026-01-29 06:05:55 -0600

    Created delete tasks provider file

---
+++ fullstack-node/src/tasks/providers/deleteTask.provider.js
@@ -0,0 +1,5 @@
+const Task = require('../task.schema.js');
+async function deleteTaskProvider(req, res) {
+    return await Task.deleteOne({_id: req.body["_id"]});
+}
+module.exports = deleteTaskProvider;
--- fullstack-node/src/tasks/tasks.controller.js
+++ fullstack-node/src/tasks/tasks.controller.js
@@ -2,7 +2,7 @@ const {StatusCodes, ReasonPhrases} = require("http-status-codes");
 const createTaskProvider = require("./providers/createTask.provider.js");
 const getTasksProvider = require("./providers/getTasks.provider.js");
 const updateTaskProvider = require("./providers/updateTask.provider.js");
-// const deleteTaskProvider = require("./providers/deleteTask.provider.js");
+const deleteTaskProvider = require("./providers/deleteTask.provider.js");
 
 async function handleGetTasks(req, res) {
     const tasks = await getTasksProvider();
@@ -19,8 +19,9 @@ async function handlePatchTasks(req, res) {
     res.status(StatusCodes.OK).json(updatedTask);
 }
 
-function handleDeleteTasks(req, res) {
-    res.send("DELETE task controller");
+async function handleDeleteTasks(req, res) {
+    const deleteTask = await deleteTaskProvider(req, res);
+    res.status(StatusCodes.OK).json(deleteTask);
 }
 
 module.exports = {
